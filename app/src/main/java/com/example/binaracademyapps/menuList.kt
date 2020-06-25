4d6e15ab7874a5f9da08ccf9d1054d2b9a9d3796
Fritzent <afritzent@gmail.com>
2020-06-26 03:29:45 +0700

    add the recyclerview menu

---
+++ app/src/main/java/com/example/binaracademyapps/menuList.kt
@@ -0,0 +1,3 @@
+package com.example.binaracademyapps
+
+data class MainMenu (val menuFirstName:String?, val menuSecondName:String?, val imageMenu: String?)
---
+++ app/src/main/java/com/example/binaracademyapps/menuListAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.binaracademyapps
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.binar_main_menu_section.view.*
+
+class Adapter(private  val menu:List<MainMenu>) :
+    RecyclerView.Adapter<Adapter.Holder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(LayoutInflater.from(parent.context).inflate(R.layout.binar_main_menu_section,parent,false))
+    }
+
+    override fun getItemCount(): Int = menu.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindSecondMenu(menu[position])
+//        holder.view.FirstMenuText.text = list?.get(position)?.menuFirstName
+    }
+
+    class Holder( view: View) : RecyclerView.ViewHolder(view){
+        private val secondMenuText = view.SecondMenuText
+        private val firstMenuText = view.FirstMenuText
+        private val menuImage = view.ImageMenu
+
+        fun bindSecondMenu(mainMenu: MainMenu){
+            firstMenuText.text = mainMenu.menuFirstName
+            secondMenuText.text = mainMenu.menuSecondName
+            Picasso.get().load(mainMenu.imageMenu).into(menuImage)
+        }
+    }
+}
